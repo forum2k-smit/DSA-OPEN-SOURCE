227b45bc97affc1b48e03715f464c4a84665f203
vasavraj <60337727+vasavraj@users.noreply.github.com>
2021-10-04 23:49:15 +0530

    Added Documentation to tic_tac_toe.py

--- Python_Projects/tic_tac_toe.py
+++ Python_Projects/tic_tac_toe.py
@@ -1,84 +1,65 @@
-def tic_tac_toe():
-    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    end = False
-    win_commbinations = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+from tkinter import *
+# It takes the position of the currently clicked cell and checks whether the gamestaistus  
+# True or not (i.e the game is over or not) and if the game is not over yet, it checks for the position and then if the corresponding cell is empty it changes its display value
+def click(r,c):
+    global usr
+    global gamestatus
+    #usr: this variable help us to identify the current player (x or o)
+    #gamestatus: it is a flag variable which indicates the status of the game ie the game is over or not. Gamestatus= False indicates game is over
+    if(usr=='X' and grd[r][c]==0 and gamestatus==True):
+        # grd: It is a 3×3 matrix which stores the values of all the cells
+        grd[r][c]='X'
+        usr='O'
+    #curr: It is a list of tkinter buttons which will click() everytime it is clicked
+        curr[r][c].configure(text='X',bg="white")
 
-    def draw():
-        print(board[0], board[1], board[2])
-        print(board[3], board[4], board[5])
-        print(board[6], board[7], board[8])
-        print()
+    if(usr=='O' and grd[r][c]==0 and gamestatus==True):
+        grd[r][c]='O'
+        usr='X'
+        curr[r][c].configure(text='O',bg="gray")
+    
+    checkWin()
+ #This function updates gamestatus by checking every row, column and diagonal.
+ #If every cell of a particular row, column or diagonal is same, gamestatus is set to false indicating game is over now
+def checkWin():
+    global gamestatus
+    for i in range(3):
+        if grd[0][i]==grd[1][i]==grd[2][i]!=0:
+            for j in range(3):
+                curr[j][i].config(bg="snow4",fg="white")
+            gamestatus=False
+    for i in range(3):            
+        if grd[i][0]==grd[i][1]==grd[i][2]!=0:
+            for j in range(3):
+                curr[i][j].config(bg="snow4",fg="white")
+            gamestatus=False
+    if grd[0][2]==grd[1][1]==grd[2][0]!=0:
+            curr[0][2].config(bg="snow4",fg="white")
+            curr[1][1].config(bg="snow4",fg="white")
+            curr[2][0].config(bg="snow4",fg="white")
+            gamestatus=False
+    if grd[0][0]==grd[1][1]==grd[2][2]!=0:
+            for j in range(3):
+                curr[j][j].config(bg="snow4",fg="white")
+            gamestatus=False
+ #to create GUI       
+root=Tk()
+root.title("Tic Tac Toe")
 
-    def p1():
-        n = choose_number()
-        if board[n] == "X" or board[n] == "O":
-            print("\nYou can't go there. Try again")
-            p1()
-        else:
+usr="O"
+gamestatus=True
+grd=[[0,0,0],
+     [0,0,0],
+     [0,0,0]]
 
-             board[n] = "X"
-           
-    def p2():
-        n = choose_number()
-        if board[n] == "X" or board[n] == "O":
-            print("\nYou can't go there. Try again")
-            p2()
-        else:
-            board[n] = "O"
+curr=[[0,0,0],
+     [0,0,0],
+     [0,0,0]]
 
-    def choose_number():
-        while True:
-            while True:
-                a = input()
-                try:
-                    a  = int(a)
-                    a -= 1
-                    if a in range(0, 9):
-                        return a
-                    else:
-                        print("\nThat's not on the board. Try again")
-                        continue
-                except ValueError:
-                   print("\nThat's not a number. Try again")
-                   continue
+for i in range(3):
+    for j in range(3):
+        curr[i][j]=Button(font=('arial',30,'bold'),width=4,
+        command=lambda r=i, c=j:click(r,c))
+        curr[i][j].grid(row=i,column=j)
 
-    def check_board():
-        count = 0
-        for a in win_commbinations:
-            if board[a[0]] == board[a[1]] == board[a[2]] == "X":
-                print("Player 1 Wins!\n")
-                print("Congratulations!\n")
-                return True
-
-            if board[a[0]] == board[a[1]] == board[a[2]] == "O":
-                print("Player 2 Wins!\n")
-                print("Congratulations!\n")
-                return True
-        for a in range(9):
-            if board[a] == "X" or board[a] == "O":
-                count += 1
-            if count == 9:
-                print("The game ends in a Tie\n")
-                return True
-
-    while not end:
-        draw()
-        end = check_board()
-        if end == True:
-            break
-        print("Player 1 choose where to place a cross")
-        p1()
-        print()
-        draw()
-        end = check_board()
-        if end == True:
-            break
-        print("Player 2 choose where to place a nought")
-        p2()
-        print()
-
-    if input("Play again (y/n)\n") == "y":
-        print()
-        tic_tac_toe()
-
-tic_tac_toe()
+mainloop()
