f652b6ca13e949c17a677c94ba23207610f7b48f
Vivaan Shiromani <shiromanivivan@gmail.com>
2021-10-04 22:15:10 +0530

    Written the required code in swap_two_without_third_var.py

---
+++ Program to Swap two numbers without using the third variable/Swap_Two_without_third_var.py
@@ -0,0 +1,19 @@
+# Done By :: Vivaan Shiromani 
+# Date :: 10th Oct 2021
+
+# Before Swap
+x=4
+y=3
+# -------
+# After swap
+# x=3
+# y=4
+
+# How to implement in python? 
+# Simply use the built-in method 
+# left, right = right, left
+
+x, y = y, x
+
+print("x=",x)
+print("y=",y)
